4ea3f0d9ab9088e45bd8482c21e4bcc378090e44
Murali S <muralisc@gmail.com>
2014-11-21 03:50:49 +0530

    added yaourtrc

--- updateFromHome.sh
+++ updateFromHome.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 cp ~/.aliases.sh ./
 cp ~/.bashrc ./
 cp ~/.bcrc ./
 cp ~/.inputrc ./
 cp ~/.vimrc ./
 cp ~/.Xresources ./
+cp ~/.yaourtrc ./
 cp ~/.zshrc ./
 
 mkdir -p .config/awesome
