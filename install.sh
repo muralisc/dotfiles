e1e8d7ec8bb88c27eb5014d5035d571e24c1648e
Murali S <muralisc@gmail.com>
2015-03-06 23:59:19 +0530

    install script for dotfiles

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mv ~/.aliases.sh  ~/.aliases.sh.bak
+mv ~/.bashrc      ~/.bashrc.bak
+mv ~/.bcrc        ~/.bcrc.bak
+mv ~/.inputrc     ~/.inputrc.bak
+mv ~/.vimrc       ~/.vimrc.bak
+mv ~/.Xresources  ~/.Xresources.bak
+mv ~/.zshrc       ~/.zshrc.bak
+mv ~/.xinitrc     ~/.xinitrc.bak
+mv ~/.muttrc      ~/.muttrc.bak
+mv ~/.fehbg       ~/.fehbg.bak
+
+
+
+ln -s `pwd`/.aliases.sh ~/.aliases.sh
+ln -s `pwd`/.bashrc     ~/.bashrc
+ln -s `pwd`/.bcrc       ~/.bcrc
+ln -s `pwd`/.inputrc    ~/.inputrc
+ln -s `pwd`/.vimrc      ~/.vimrc
+ln -s `pwd`/.Xresources ~/.Xresources
+ln -s `pwd`/.zshrc      ~/.zshrc
+ln -s `pwd`/.xinitrc    ~/.xinitrc
+ln -s `pwd`/.muttrc     ~/.muttrc
+ln -s `pwd`/.fehbg      ~/.fehbg
