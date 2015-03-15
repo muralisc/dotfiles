ca1e956b31aa25a66ac1f0d86fb2fc355d84a57a
Murali S <muralisc@gmail.com>
2015-03-16 01:20:25 +0530

    touch

--- updateFromHome.sh
+++ updateFromHome.sh
@@ -10,10 +10,10 @@ cp ~/.xinitrc ./
 cp ~/.muttrc ./
 cp ~/.fehbg ./
 
-mkdir -p .config/
 cp -r ~/.config/awesome ./.config/
 cp -r ~/.config/zathura ./.config/
 cp -r ~/.config/user-dirs.dirs ./.config/
+cp -r ~/.ssh/config ./.ssh/
 
 cp -r ~/.mutt ./
 cp -r ~/bin ./
