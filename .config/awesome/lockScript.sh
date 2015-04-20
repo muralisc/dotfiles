dbf1489b604a2e7d5b3690f1b72627e43bd4d03c
Murali Suresh <muralisc@gmail.com>
2015-04-20 16:54:09 +0530

    document config

--- .config/awesome/lockScript.sh
+++ .config/awesome/lockScript.sh
@@ -6,12 +6,13 @@
 # mogrify -scale 10% -scale 1000% ~/Downloads/wallpaperLock.png
 # convert -resize 1920 bb9a5897307ea096bd66dd658dfa1d75.jpg wallpaperLock.png
 ######## 1920x1080
+Wallpaper_folder=~/PersonalData_max26gb/Jpegs/Wallpapers/
 if [ `wc -l ~/Downloads/wallpaper_list | cut -d " " -f1` -eq 0 ];
 then
     # execution here means no lines in file
 
     # get the files and shuffle them into the wallpaper list
-    \find ~/PersonalData_max26gb/Wallpaper/Widescreen_HD_Nature -iname "*.jpg" -o -iname "*.png" |shuf > ~/Downloads/wallpaper_list
+    \find $Wallpaper_folder -iname "*.jpg" -o -iname "*.png" |shuf > ~/Downloads/wallpaper_list
 fi
 # Lock screen displaying this image.
 FILE=`head -1 ~/Downloads/wallpaper_list`
