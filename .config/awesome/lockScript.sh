8fc62b2a9e46ab8ce3436c19946cb3c357ab46ab
Murali Suresh <muralisc@gmail.com>
2016-02-03 02:10:14 +0530

    moved paths to variables

--- .config/awesome/lockScript.sh
+++ .config/awesome/lockScript.sh
@@ -1,25 +1,25 @@
 #!/bin/sh -e
 
 ######## Take a screenshot
-# scrot /tmp/wallpaperLock.png
+# scrot $FINAL_PATH
 ######## Pixellate it 10x
-# mogrify -scale 10% -scale 1000% /tmp/wallpaperLock.png
+# mogrify -scale 10% -scale 1000% $FINAL_PATH
 # convert -resize 1920 bb9a5897307ea096bd66dd658dfa1d75.jpg wallpaperLock.png
 ######## 1920x1080
 Wallpaper_folder=~/PersonalData_max26gb/Jpegs/Quotataions/
-touch /var/tmp/wallpaper_list
-if [ `wc -l /var/tmp/wallpaper_list | cut -d " " -f1` -eq 0 ];
+LIST_PATH=/var/tmp/wallpaper_list
+FINAL_PATH=/tmp/wallpaperLock.png
+touch $LIST_PATH
+if [ `wc -l $LIST_PATH | cut -d " " -f1` -eq 0 ];
 then
     # execution here means no lines in file
-
     # get the files and shuffle them into the wallpaper list
-    # \find $Wallpaper_folder -iname "*.jpg" -o -iname "*.png" |shuf > /var/tmp/wallpaper_list
-    \find $Wallpaper_folder -iregex ".*\(txt\|png\|jpg\|svg\)" |shuf > /var/tmp/wallpaper_list
+    \find $Wallpaper_folder -iregex ".*\(txt\|png\|jpg\|svg\)" |shuf > $LIST_PATH
 fi
 # Lock screen displaying this image.
-FILE=`head -1 /var/tmp/wallpaper_list`
-sed -i "1d" /var/tmp/wallpaper_list
-echo "$FILE" > /tmp/lastLock
+FILE=`head -1 $LIST_PATH`
+sed -i "1d" $LIST_PATH
+echo "$FILE" > /tmp/lastLock.log
 
 # check if file is not JPG or PNG make png.
 
@@ -29,8 +29,8 @@ extension="${filename##*.}"
 # if svg
 if [ "$extension" == "svg" ];
 then
-    inkscape $FILE -e /tmp/wallpaperLock.png
-    FILE=/tmp/wallpaperLock.png
+    inkscape $FILE -e $FINAL_PATH
+    FILE=$FINAL_PATH
     echo "convert svg"
 fi
 # if txt
@@ -38,9 +38,9 @@ if [ "$extension" == "txt" ];
 then
     pushd $Wallpaper_folder
     ./printSvgFromFile.py $FILE > /tmp/wallpconvert.svg
-    inkscape /tmp/wallpconvert.svg -e /tmp/wallpaperLock.png
+    inkscape /tmp/wallpconvert.svg -e $FINAL_PATH
     popd
-    FILE=/tmp/wallpaperLock.png
+    FILE=$FINAL_PATH
     echo "convert txt"
 fi
 
@@ -54,18 +54,18 @@ y=`echo $res | grep -Po "(?<=x)[0-9]+"`
 # resize to x
 if (( "$y" > "$x" )) 
 then
-    convert -resize 1920  $FILE /tmp/wallpaperLock.png
-    echo "resized to x 1920" >> /tmp/lastLock
+    convert -resize 1920  $FILE $FINAL_PATH
+    echo "resized to x 1920" >> /tmp/lastLock.log
 else
     # else rezize to y
-    echo "resized to y 1080" >> /tmp/lastLock
-    convert -resize x1080  $FILE /tmp/wallpaperLock.png
-    color=`convert -resize 1x1 /tmp/wallpaperLock.png txt:-| grep -Po '#[0-9A-F]+'`
+    echo "resized to y 1080" >> /tmp/lastLock.log
+    convert -resize x1080  $FILE $FINAL_PATH
+    color=`convert -resize 1x1 $FINAL_PATH txt:-| grep -Po '#[0-9A-F]+'`
     convert -size 1920x1080 xc:"$color" background.png
-    composite -gravity Center /tmp/wallpaperLock.png background.png /tmp/wallpaperLock.png
+    composite -gravity Center $FINAL_PATH background.png $FINAL_PATH
 fi
 # use the file
-i3lock -i /tmp/wallpaperLock.png
+i3lock -i $FINAL_PATH
 
 # Turn the screen off after a delay.
 sleep 60; pgrep i3lock && xset dpms force off
