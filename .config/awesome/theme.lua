886b8671d778cfa3b00afb196bc6ba43a3c99cad
Murali Suresh <muralisc@gmail.com>
2016-05-19 17:04:23 +0530

    change wallpaper location

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -75,7 +75,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper="/home/murali/PersonalData_max26gb/Jpegs/Quotataions/black-wallpaper-hd.jpg"
+theme.wallpaper="/home/murali/PersonalData_max26gb/Jpegs/black-wallpaper-hd.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
