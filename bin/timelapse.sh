1347b30b697a6353848b17f81e99b5b09d907b91
Murali Suresh <muralisc@gmail.com>
2016-06-13 20:42:01 +0530

    timelapse udpate

--- bin/timelapse.sh
+++ bin/timelapse.sh
@@ -5,11 +5,16 @@
 
 export DISPLAY=:0
 # take 4 pics in a minute
-for i in 1 2 3 4
+NOPICS=4
+for i in `seq 1 $NOPICS`
 do
 dir=`who -b | awk '{print $3"_"$4}'` #get log in time
 mkdir -p ~/Downloads/tl/$dir
-ffmpeg -f video4linux2 -i /dev/video0 -vframes 1 ~/Downloads/tl/$dir/$(date +\%Y-\%m-\%d-\%H-%M)_${i}_tl.jpg
-import -window root ~/Downloads/tl/${dir}/$(date +%Y-%m-%d-%H-%M)_${i}_scrot.jpg
-sleep 14s
+ffmpeg -f video4linux2 -i /dev/video0 -vframes 1 ~/Downloads/tl/$dir/$(date +\%Y-\%m-\%d-\%H-%M)_${i}_tl.jpg >> /tmp/tl.log
+if [[ -a /dev/video0 ]]; then
+    #if video device exites
+    import -window root ~/Downloads/tl/${dir}/$(date +%Y-%m-%d-%H-%M)_${i}_scrot.jpg >> /tmp/tl.log
+fi
+sleep $((60/$NOPICS -1))s
 done 
+# vim: set tw=200 :
