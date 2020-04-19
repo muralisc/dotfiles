698f289de39301dd96b2b0f9653741d043dbb6d0
Murali S <muralisc@gmail.com>
2020-04-19 11:37:25 +0530

    Fix script errors

--- bin/encode.sh
+++ bin/encode.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Command details:
+#  time ffmpeg -i $filename                       \
+#      -copy_unknown                              \ # Copy Unknown streams
+#      -map_metadata 0                            \ # map metadat from inp file
+#      -codec copy                                \ # copy data streams
+#      -codec:v libx264 -pix_fmt yuvj420p -crf 23 \ # encode video using libx264 with pix_fmt yuv420p and cfr of 23
+#      -codec:a aac -vbr 4                        \ # encode audio using aac adn vbr of 4
+#      -preset medium ${DEST_DIR}/${filename%.*}_encoded.mp4
 
-$DEST_DIR=$1
+DEST_DIR=$1
 shift
 for filename in $@ ; do
-    time ffmpeg -i $filename                      \
-        -copy_unknown                             \  # Copy Unknown streams
-        -map_metadata 0                           \  # map metadat from inp file
-        -codec copy                               \  # copy data streams
-        -codec:v libx264 -pix_fmt yuv420p -crf 23 \  # encode video using libx264 with pix_fmt yuv420p and cfr of 23
-        -codec:a aac -vbr 4                       \  # encode audio using aac adn vbr of 4
-        -preset medium ${DEST_DIR}/${filename%.*}_encoded.mp4
+    echo "Starting to encode File: $filename :"
+    time ffmpeg -i $filename                       \
+        -copy_unknown                              \
+        -map_metadata 0                            \
+        -codec copy                                \
+        -codec:v libx264 -pix_fmt yuvj420p -crf 23 \
+        -codec:a aac -vbr 4                        \
+        -preset fast ${DEST_DIR}/${filename%.*}_encoded.mp4
 done
