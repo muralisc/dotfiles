1eb550a3d64bb60727db09466084e270aeac4b2a
Murali S <muralisc@gmail.com>
2020-04-19 11:26:46 +0530

    Add doc

--- bin/encode.sh
+++ bin/encode.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+
 $DEST_DIR=$1
 shift
 for filename in $@ ; do
-    ffmpeg -i $filename \
-        -copy_unknown -map_metadata 0 -codec copy \
-        -codec:v libx264 -pix_fmt yuv420p -crf 23 \
-        -codec:a aac -vbr 4 \
+    ffmpeg -i $filename                           \
+        -copy_unknown                             \  # Copy Unknown streams
+        -map_metadata 0                           \  # map metadat from inp file
+        -codec copy                               \  # copy data streams
+        -codec:v libx264 -pix_fmt yuv420p -crf 23 \  # encode video using libx264 with pix_fmt yuv420p and cfr of 23
+        -codec:a aac -vbr 4                       \  # encode audio using aac adn vbr of 4
         -preset medium ${DEST_DIR}/${filename%.*}_encoded.mp4
 done
