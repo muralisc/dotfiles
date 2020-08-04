4ebf256709e5f5bb4a257570d4750db3199575a8
Murali S <muralisc@gmail.com>
2020-08-04 23:53:17 +0530

    Update script to use quotes

--- bin/encode.sh
+++ bin/encode.sh
@@ -11,15 +11,15 @@
 #      -codec:a aac -vbr 4                        \ # encode audio using aac adn vbr of 4
 #      -preset medium ${DEST_DIR}/${filename%.*}_encoded.mp4
 
-DEST_DIR=$1
+DEST_DIR=${1:-$PWD}
 shift
 for filename in $@ ; do
-    echo "Starting to encode File: $filename :"
-    time ffmpeg -i $filename                       \
+    echo "Starting to encode File: $filename at $(date):"
+    time ffmpeg -i "$filename"                       \
         -copy_unknown                              \
         -map_metadata 0                            \
         -codec copy                                \
         -codec:v libx264 -pix_fmt yuvj420p -crf 23 \
         -codec:a aac -vbr 4                        \
-        -preset fast ${DEST_DIR}/${filename%.*}_encoded.mp4
+        -preset fast "${DEST_DIR}/${filename%.*}_encoded.mp4"
 done
