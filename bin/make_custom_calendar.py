bee6b3d95dc81109c5cbcabfde3b8a96860f3343
muralisc <muralisc@gmail.com>
2023-06-11 19:34:40 +0100

    Update cront

--- bin/make_custom_calendar.py
+++ bin/make_custom_calendar.py
@@ -12,7 +12,7 @@ def add_events(start, count, cal, session_start):
     event_no = start
     for rep in range(count):
         event = Event()
-        event.add("summary", f"[3] {event_no}/8 Christian Physical Training")
+        event.add("summary", f"[8] {event_no}/8 Christian Physical Training")
         event.add("dtstart", next_session)
         event.add("dtend", next_session + timedelta(hours=1))
         event.add("dtstamp", datetime.now())
@@ -22,11 +22,11 @@ def add_events(start, count, cal, session_start):
 
 
 cal = Calendar()
-wed_session_start = datetime(2022, 12, 7, 7, 0, 0, tzinfo=pytz.utc)  # Wednesdays
-sat_session_start = datetime(2022, 12, 10, 8, 0, 0, tzinfo=pytz.utc)  # Saturdays
+wed_session_start = datetime(2023, 5, 30, 7, 30, 0, tzinfo=pytz.utc)  # Tuesdays
+sat_session_start = datetime(2023, 5, 26, 7, 30, 0, tzinfo=pytz.utc)  # Friday
 
-add_events(start=1, count=4, cal, wed_session_start)
-add_events(start=2, count=4, cal, sat_session_start)
+add_events(start=2, count=4, cal=cal, session_start=wed_session_start)
+add_events(start=1, count=4, cal=cal, session_start=sat_session_start)
 
 directory = os.path.expanduser("~")
 f = open(os.path.join(directory, "physical_training.ics"), "wb")
