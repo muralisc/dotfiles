2855a6475db270c56a094c428dbf569fda48ab13
muralisc <muralisc@gmail.com>
2024-04-03 22:37:36 +0100

    Add ssh-config creator

---
+++ bin/make-ssh-config-from-nmap.py
@@ -0,0 +1,26 @@
+
+import sys
+
+# Usage: bash bin/nmap-parse-output | python bin/make-ssh-config-from-nmap.py
+
+if not sys.stdin.isatty():
+   f = sys.stdin 
+else:
+   f = open(sys.argv[1])
+
+list_of_configs = [
+        {"mac_id": "60:F2:62:C1:26:A9", "pref_hostname": "bonjikka"}
+        ]
+
+for line in f:
+    for config in list_of_configs:
+        if line.count(config["mac_id"]) > 0:
+            columns =line.split(" ")
+            print(columns)
+            ip_addr = columns[4]
+            pref_hostname = config["pref_hostname"]
+            print(f''' 
+Host {pref_hostname}
+    HostName {ip_addr} 
+    User murali
+''')
