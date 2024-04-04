f55e5a93decf7931e817ef0ed966124768ee0b82
muralisc <muralisc@gmail.com>
2024-04-04 10:36:46 +0100

    Move video and photo scripts to github.com/muralisc/raspberrypi-photoframe

--- bin/export_emacs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p ~/public_html/emacs/
-emacs -batch -l ~/.emacs -eval '(org-batch-store-agenda-views)'
-emacs -l ~/.emacs -batch -eval '(progn
-        (org-batch-agenda-csv "a"
-       org-agenda-span (quote year)))' | awk -F"," '{print $1","$6","$2}' | column -t -s, > ~/public_html/emacs/agenda-csv-export.txt
-emacs -batch -l ~/.emacs -eval '(org-batch-agenda "a" org-agenda-span 30)' >> ~/public_html/emacs/agenda-csv-export.txt
