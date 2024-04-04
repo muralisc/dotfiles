b0f46dd383dbda745edd5726aa29fb33d08e9e25
muralisc <muralisc@gmail.com>
2024-04-05 00:27:42 +0100

    Update find deleted script

--- bin/py-scripts/find-not-existing-in-converted-sqlite.py
+++ bin/py-scripts/find-not-existing-in-converted-sqlite.py
@@ -7,28 +7,33 @@ import click
 
 database_proxy = DatabaseProxy()
 
+
 class BaseModel(Model):
     class Meta:
         database = database_proxy
 
+
 class Files(BaseModel):
     abs_path = CharField(unique=True)
     index_timestamp = DateTimeField()
+
 
 class Events(BaseModel):
     event_type = CharField()
     timestamp = DateTimeField()
     abs_path = CharField()
 
+
 @click.group()
 @click.pass_context
 def main(ctx):
     pass
 
+
 @main.command()
 @click.pass_context
-@click.option('--sqlite', required=True, help='Path to sqlite db')
-@click.option('--source-dir', required=True, help='Path to directory to add to index')
+@click.option("--sqlite", required=True, help="Path to sqlite db")
+@click.option("--source-dir", required=True, help="Path to directory to add to index")
 def index(ctx, sqlite, source_dir):
     database = SqliteDatabase(sqlite)
     database_proxy.initialize(database)
@@ -46,19 +51,29 @@ def index(ctx, sqlite, source_dir):
         # for file in Files.select():
         #     print(f"db row: {file.abs_path}, {file.index_timestamp}")
 
+
 @main.command()
 @click.pass_context
-@click.option('--sqlite', required=True, help='Path to sqlite db')
-@click.option('--prefix', required=True, help='Prefix of filepath to remove from index path')
-@click.option('--dest-dir', required=True, help='Desitination folder containing file source')
+@click.option("--sqlite", required=True, help="Path to sqlite db")
+@click.option(
+    "--prefix", required=True, help="Prefix of filepath to remove from index path"
+)
+@click.option(
+    "--dest-dir", required=True, help="Desitination folder containing file source"
+)
 def find_deleted(ctx, sqlite, prefix, dest_dir):
     database = SqliteDatabase(sqlite)
+    database_proxy.initialize(database)
     with database:
-        for copy_filepath in Files.select():
-            source_path = copy_filepath.replace(prefix,dest_dir)
-            print(f"copy_filepath: {copy_filepath}, source_path: {source_path}"
-            if Path(copy_filepath).exist() is False and Path(source_path).exist() is True:
-                print(f"To be delted: {source_path}")
+        for copy_file_row in Files.select():
+            copy_filepath = str(copy_file_row.abs_path)
+            source_path = copy_filepath.replace(prefix, dest_dir)
+            if (
+                Path(copy_filepath).exists() is False
+                and Path(source_path).exists() is True
+            ):
+                print(f"{source_path}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
