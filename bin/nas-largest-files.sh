60d17d6ddb8715025f016dc9dcdcdca6a4ca0994
muralisc <muralisc@gmail.com>
2024-04-26 09:27:59 +0100

    Organize folder

--- bin/nas-largest-files.sh
+++ bin/nas-largest-files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "use storage analyzer if possible"
+read
  find $PWD -type f -size +1M -printf "%s\t%p\n" | sort -n > /tmp/nas-files.log 
  tail -10 /tmp/nas-files.log | numfmt --to=iec-i
